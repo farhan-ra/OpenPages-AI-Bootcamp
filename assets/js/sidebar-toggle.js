2175c771438d536a35e0822a4c969b145ba43288
farhan-ra <farhan-ra@users.noreply.github.com>
2025-10-22 02:43:13 +0000

    deploy: b7fcc103df2a7432bac20dc2f1090f7547cd5450

---
+++ assets/js/sidebar-toggle.js
@@ -0,0 +1,7 @@
+window.addEventListener("DOMContentLoaded", function () {
+  const navToggle = document.querySelector(".md-header__button.md-icon--menu");
+
+  if (navToggle) {
+    navToggle.style.display = "block";
+  }
+});
